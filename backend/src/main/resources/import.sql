a9c644880f0690c96259e1094198570e2bbadbdf
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-08-14 17:39:41 -0300

    Resource, ResourceType and SEED

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -16,3 +16,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Google Course', 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 'https://iconscout.com/icon/certificate-3155213');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha', 'Trilha do curso', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, 1)
+
