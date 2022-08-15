fc6ba795bef2801ee262e5ab29b44aae6f3c5e16
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-08-15 13:15:54 -0300

    Section and SEED

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -16,6 +16,12 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Google Course', 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 'https://iconscout.com/icon/certificate-3155213');
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 1);
 
-INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha', 'Trilha do curso', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, 1)
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha', 'Trilha do curso', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 0, 1);
 
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Básico 1', 1, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Básico 2', 2, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Básico 3', 3, 'https://www.trustedreviews.com/news/google-building-health-tracking-platform-google-fit-2918300', 1, 2);
