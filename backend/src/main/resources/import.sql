8489bfb546e869bc564557347f772f5ff949dd79
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-08-18 13:25:31 -0300

    Lesson SEED and fix: lessonDone

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -29,7 +29,20 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', null, true, false)
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', null, true, false)
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 1 do Capítulo 1', 1, 1)
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Materia de apoio', 'https://www.youtube.com/watch?v=b3lH6CULHD0&list=RDb3lH6CULHD0&start_radio=1');
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 2 do Capítulo 1', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'Materia de apoio', 'https://www.youtube.com/watch?v=b3lH6CULHD0&list=RDb3lH6CULHD0&start_radio=1');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Aula 3 do Capítulo 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, 'Materia de apoio', 'https://www.youtube.com/watch?v=b3lH6CULHD0&list=RDb3lH6CULHD0&start_radio=1');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa do Capítulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Tarefa', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z')
+
+INSERT INTO tb_lesson_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1)
+INSERT INTO tb_lesson_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1)
 
 
 
